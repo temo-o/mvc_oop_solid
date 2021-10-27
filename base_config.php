df54829e0ffaa26a49c82d8705678d6a7367ee25
Temur Topuria <temur_topuria@epam.com>
2021-10-27 11:14:55 +0400

    Ignore 6

--- base_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    define("BASE_URL", __dir__);
-    define("REQUEST_URI", $_SERVER["REQUEST_URI"]);
-    define("BASE_FOLDER", "/trips_04/mvc_oop_solid");
-
-    define("unwanted", "/trips_04/mvc_oop_solid");
-
-?>
